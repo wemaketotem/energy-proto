de74d373ce34dd32f5d1257af94a6f677f010893
Diderik <diderik@think-innovation.com>
2015-06-20 17:09:49 +0200

    Initial version, LED blinks on start-up and a few times every second after that

--- firmware/gateway/gateway.ino
+++ firmware/gateway/gateway.ino
@@ -1,7 +1,8 @@
-#include <SoftwareSerial.h>
+// LED only check
+//#include <SoftwareSerial.h>
 
-#include <EtherCard.h>
-#include <SoftwareSerial.h>
+//#include <EtherCard.h>
+//#include <SoftwareSerial.h>
 
 // Define all digital pins used
 #define requestPin  6  // P1 request line
@@ -10,34 +11,34 @@
 #define ledPin      15 // LED
 
 // Ethernet configuration constants, change these settings to match your own setup
-static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 }; // ethernet interface mac address, must be unique on the LAN
-const char website[] PROGMEM = "emoncms.org";
-#define APIKEY "adc984f0efa3f9d6114b6677c6f08cd3" // Robert
+//static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 }; // ethernet interface mac address, must be unique on the LAN
+//const char website[] PROGMEM = "emoncms.org";
+//#define APIKEY "adc984f0efa3f9d6114b6677c6f08cd3" // Robert
 //#define APIKEY "121ac49b2af30c3c1bd82110dd877c52" // Marten
 
 // Ethernet variables
-byte Ethernet::buffer[350]; // The buffer used by the Ethernet stack
-Stash stash; // The buffer that contains the POST data
-byte session; // Session identification
-int res = 0; //timing variable for resetting the Ethernet stack
+//byte Ethernet::buffer[350]; // The buffer used by the Ethernet stack
+//Stash stash; // The buffer that contains the POST data
+//byte session; // Session identification
+//int res = 0; //timing variable for resetting the Ethernet stack
 
 // P1 hardware configuration
-SoftwareSerial mySerial(rxPin, txPin, true); // RX, TX, inverted
+//SoftwareSerial mySerial(rxPin, txPin, true); // RX, TX, inverted
 
 bool ledOn = false;
 
 void setup () {
   // Configure debug serial output
   Serial.begin(57600);
-  Serial.println("\n[Xively example]");
+  Serial.println("\nLED only check");
 
   // Initialize ethernet, it is blocking until it receives a DHCP lease
-  initialize_ethernet();
+  //initialize_ethernet();
 
   // Configure P1 port pinning
-  mySerial.begin(9600);
-  pinMode(requestPin, OUTPUT);
-  digitalWrite(requestPin, HIGH);
+  //mySerial.begin(9600);
+  //pinMode(requestPin, OUTPUT);
+  //digitalWrite(requestPin, HIGH);
   
   // Configure LED
   pinMode(ledPin, OUTPUT);
@@ -52,27 +53,40 @@ void setup () {
 
 
 void loop () { 
-    while (mySerial.available()) {
+    /*while (mySerial.available()) {
       int incomingByte = mySerial.read();
       ledOn = !ledOn;
       digitalWrite(ledPin, ledOn); // Toggle LED on each received byte
-    }
-    digitalWrite(ledPin, HIGH); // Make sure the led is off when no incomming P1 bytes to handle
+    }*/
+    //digitalWrite(ledPin, HIGH); // Make sure the led is off when no incomming P1 bytes to handle
  
-    ether.packetLoop(ether.packetReceive()); // Need to be touched continuesly
+    //ether.packetLoop(ether.packetReceive()); // Need to be touched continuesly
 
     //if correct answer is not received then re-initialize ethernet module
-    if (res > 220){
+    /*if (res > 220){
       initialize_ethernet(); 
+    }*/
+    //res = res + 1;
+
+    // Let LED pulse 2 times on 10Hz at boot
+    for (int i = 0; i < 2; i++) {
+      digitalWrite(ledPin, LOW);
+      delay(100);
+      digitalWrite(ledPin, HIGH);
+      delay(100);
     }
-    res = res + 1;
-    delay(50);
+
+    Serial.print("RAM: ");
+    Serial.println(freeRam()); 
+
+    delay(500);
+
 } 
 
 
 
 
-
+/*
 void initialize_ethernet(void){  
   for(;;){ // keep trying until you succeed 
     //Reinitialize ethernet module
@@ -111,6 +125,7 @@ void initialize_ethernet(void){
     Serial.print("RAM: ");
     Serial.println(freeRam()); 
 }
+*/
 
 int freeRam() {
   extern int __heap_start, *__brkval; 
