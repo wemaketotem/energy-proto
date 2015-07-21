4ffcfd4ff960f5514142bf87804af4911a3b1049
Marten Lootsma <m.lootsma@gmail.com>
2015-07-21 22:36:47 +0200

    Disabled ethernet, set pinning for the software serial for P1 input

--- firmware/gateway/gateway.ino
+++ firmware/gateway/gateway.ino
@@ -1,13 +1,10 @@
-#include <SoftwareSerial.h>
-
 #include <EtherCard.h>
 #include <SoftwareSerial.h>
-
 // Define all digital pins used
 #define requestPin  6  // P1 request line
-#define rxPin       4  // P1 UART RX
+#define rxPin       7  // P1 UART RX
 #define txPin       5  // Unused TX (is needed for SoftwareSerial)
-#define ledPin      15 // LED
+#define ledPin      13 // LED
 
 // Ethernet configuration constants, change these settings to match your own setup
 static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 }; // ethernet interface mac address, must be unique on the LAN
@@ -23,7 +20,6 @@ int res = 0; //timing variable for resetting the Ethernet stack
 
 // P1 hardware configuration
 SoftwareSerial mySerial(rxPin, txPin, true); // RX, TX, inverted
-
 // P1 parsing variables
 String inputString = ""; // A string object that will contain one P1 message line
 String P181, P182, P281, P282, P170, P270, G; // The energy value strings cut from the P1 message
@@ -40,7 +36,7 @@ void setup () {
   Serial.println("\n[EmonCMS example]");
 
   // Initialize ethernet, it is blocking until it receives a DHCP lease
-  initialize_ethernet();
+//  initialize_ethernet();
 
   // Configure P1 port pinning
   mySerial.begin(9600);
@@ -135,37 +131,37 @@ void loop () {
      
       Serial.print("RAM: ");
       Serial.println(freeRam()); 
-     
-      // Build the json structure
-      byte sd = stash.create();
-      stash.print("{181:");
-      stash.print(P181);
-      stash.print(",182:");
-      stash.print(P182);
-      stash.print(",281:");
-      stash.print(P281);
-      stash.print(",282:");
-      stash.print(P282);
-      stash.print(",170:");
-      stash.print(P170);
-      stash.print(",270:");
-      stash.print(P270);
-      stash.print(",gas:");
-      stash.print(G);
-      stash.print("}");
-      stash.save();
-      int stash_size = stash.size();
-      
-      // Build the header with json structure in the URL
-      Stash::prepare(PSTR("GET http://$F/input/post.json?json=$H&apikey=$F HTTP/1.0" "\r\n"
-        "Host: $F" "\r\n"
-        "Content-Length: 0" "\r\n"
-        "\r\n"
-        ""), website, sd, PSTR(APIKEY), website);
-      
-      // send the packet - this also releases all stash buffers once done
-      session = ether.tcpSend();
-      isSending = true;        
+//     
+//      // Build the json structure
+//      byte sd = stash.create();
+//      stash.print("{181:");
+//      stash.print(P181);
+//      stash.print(",182:");
+//      stash.print(P182);
+//      stash.print(",281:");
+//      stash.print(P281);
+//      stash.print(",282:");
+//      stash.print(P282);
+//      stash.print(",170:");
+//      stash.print(P170);
+//      stash.print(",270:");
+//      stash.print(P270);
+//      stash.print(",gas:");
+//      stash.print(G);
+//      stash.print("}");
+//      stash.save();
+//      int stash_size = stash.size();
+//      
+//      // Build the header with json structure in the URL
+//      Stash::prepare(PSTR("GET http://$F/input/post.json?json=$H&apikey=$F HTTP/1.0" "\r\n"
+//        "Host: $F" "\r\n"
+//        "Content-Length: 0" "\r\n"
+//        "\r\n"
+//        ""), website, sd, PSTR(APIKEY), website);
+//      
+//      // send the packet - this also releases all stash buffers once done
+//      session = ether.tcpSend();
+//      isSending = true;        
       
       // Message handled
       msgComplete = false;
@@ -174,69 +170,66 @@ void loop () {
       Serial.println(freeRam()); 
    }
  
-   if (isSending) {
-     const char* reply = ether.tcpReply(session);
-     
-     if (reply != 0) {
-       res = 0;
-       Serial.println("Respone");
-       Serial.println(reply);
-       isSending = false;
-     } else {
-        //if correct answer is not received then re-initialize ethernet module
-        if (res > 220){
-          initialize_ethernet(); 
-        }
-        res = res + 1;
-        delay(50);
-     }
-   }
-
-   ether.packetLoop(ether.packetReceive()); // Need to be touched continuesly
+//   if (isSending) {
+//     const char* reply = ether.tcpReply(session);
+//     
+//     if (reply != 0) {
+//       res = 0;
+//       Serial.println("Respone");
+//       Serial.println(reply);
+//       isSending = false;
+//     } else {
+//        //if correct answer is not received then re-initialize ethernet module
+//        if (res > 220){
+//          initialize_ethernet(); 
+//        }
+//        res = res + 1;
+//        delay(50);
+//     }
+//   }
+//
+//   ether.packetLoop(ether.packetReceive()); // Need to be touched continuesly
 }
 
-
-
-
-
-void initialize_ethernet(void){  
-  for(;;){ // keep trying until you succeed 
-    //Reinitialize ethernet module
-    pinMode(5, OUTPUT);
-    Serial.println("Reseting Ethernet...");
-    digitalWrite(5, LOW);
-    delay(1000);
-    digitalWrite(5, HIGH);
-    delay(500);
-
-    if (ether.begin(sizeof Ethernet::buffer, mymac) == 0){ 
-      Serial.println( "Failed to access Ethernet controller");
-      continue;
-    }
-    
-    if (!ether.dhcpSetup()){
-      Serial.println("DHCP failed");
-      continue;
-    }
-
-    ether.printIp("IP:  ", ether.myip);
-    ether.printIp("GW:  ", ether.gwip);  
-    ether.printIp("DNS: ", ether.dnsip);  
-
-    if (!ether.dnsLookup(website)) {
-      Serial.println("DNS failed");
-      continue;
-    } else {
-      ether.printIp("SRV: ", ether.hisip);
-  
-      //reset init value
-      res = 0;
-      break;
-    }    
-  }
-    Serial.print("RAM: ");
-    Serial.println(freeRam()); 
-}
+//
+//void initialize_ethernet(void){  
+//  for(;;){ // keep trying until you succeed 
+//    //Reinitialize ethernet module
+//    pinMode(5, OUTPUT);
+//    Serial.println("Reseting Ethernet...");
+//    digitalWrite(5, LOW);
+//    delay(1000);
+//    digitalWrite(5, HIGH);
+//    delay(500);
+//
+//    if (ether.begin(sizeof Ethernet::buffer, mymac) == 0){ 
+//      Serial.println( "Failed to access Ethernet controller");
+//      continue;
+//    }
+//    
+//    if (!ether.dhcpSetup()){
+//      Serial.println("DHCP failed");
+//      continue;
+//    }
+//
+//    ether.printIp("IP:  ", ether.myip);
+//    ether.printIp("GW:  ", ether.gwip);  
+//    ether.printIp("DNS: ", ether.dnsip);  
+//
+//    if (!ether.dnsLookup(website)) {
+//      Serial.println("DNS failed");
+//      continue;
+//    } else {
+//      ether.printIp("SRV: ", ether.hisip);
+//  
+//      //reset init value
+//      res = 0;
+//      break;
+//    }    
+//  }
+//    Serial.print("RAM: ");
+//    Serial.println(freeRam()); 
+//}
 
 int freeRam() {
   extern int __heap_start, *__brkval; 
